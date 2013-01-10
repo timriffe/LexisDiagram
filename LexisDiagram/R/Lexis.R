b55b23ab6b086d95ff2b8c6af8cd462b45692834
triffe <tim.riffe@gmail.com>
2013-01-09 21:52:48 -0800

    changed flow control to make better use of logicals

--- LexisDiagram/R/Lexis.R
+++ LexisDiagram/R/Lexis.R
@@ -1,47 +1,72 @@
 Lexis <-
-function(ages,years,labs=T,col="black"){
+function(ages,
+		years,
+		labs = TRUE,
+		col = "black",
+		open.device = TRUE,
+		mar = c(1,1,1,1),
+		age.lab = "Age",
+		year.lab = "Year",
+		cohort.lab = "Birth Cohort",
+		cex.lab = 1,
+		cex.ax.lab = 1){
 	xrange <- diff(range(years))
 	yrange <- diff(range(ages))
-	maxrange <- max(c(xrange,yrange))
-	xrange <- xrange*(5/maxrange)
-	yrange <- yrange*(5/maxrange)
-	if (labs==T){
+	maxrange <- max(c(xrange, yrange))
+	xrange <- xrange * (5 / maxrange)
+	yrange <- yrange * (5 / maxrange)
+	
+	# open a device if ecessary, set margins
+	if (open.device){
+		dev.new(width=xrange+2,height=yrange+2)
+	}
+	# start plotting
+	par(xaxs="i",yaxs="i",mar=mar)
+	
+	if (labs){
 		# x coordinate for generations
-		interval <- 5/max(c(length(ages),length(years)))
-		genx <- max(years)+ .25/interval
+		interval <- 5 / max(c(length(ages), length(years)))
+		genx <- max(years) + .25 / interval
 		# year labels don't always fit:
 		x <- years; y <- ages; xn <- length(x); yn <- length(y)
 		
-		# super-primitive guessing at which graphics device to use
-		A <- try(windows(width=xrange+2,height=yrange+2),silent=T)
-		if (!is.null(A)) A <- try(Quartz(width=xrange+2,height=yrange+2),silent=T)
-		if (!is.null(A)) A <- try(x11(width=xrange+2,height=yrange+2),silent=T)
-		# start plotting
-		par(mai=c(1,1,1,1))
-		plot(NA,type="n",xlim=c(min(x),max(x)),ylim=c(min(y),max(y)),axes=F,xlab="Years",ylab="Age",cex.lab=1.5,asp=1,col=col)
-		segments(x,min(ages),x,max(ages),col=col);segments(min(years),y,max(years),y,col=col)
-		for (i in 1:(xn-1)) {
-			segments(rep(x[i],(xn-1)),y[-yn],rep(x[i+1],(xn-1)),y[-1],col=col)
-			axis(1,at=x,labels=x,tick=F,pos=(min(y)-(.1/interval)))
-			axis(2,at=y,labels=y,tick=F,pos=(min(x)),las=2)
-			for (i in 1:yn){
-				text(genx,y[i]+1,labels=(max(x)-y[i]-1),srt=45,xpd=T)
+		plot(NA, 
+				type = "n",
+				xlim = range(x),
+				ylim = range(y),
+				axes = FALSE, 
+				xlab = year.lab,
+				ylab = age.lab,
+				cex.lab = cex.lab,
+				asp = 1,
+				col = col)
+		segments(x, min(ages), x,max(ages), col = col)
+		segments(min(years), y, max(years), y, col = col)
+		for (i in 1:(xn - 1)) {
+			segments(rep(x[i], (xn-1)), y[-yn], rep(x[i + 1], (xn - 1)), y[-1], col = col)
+			axis(1, at = x, labels = x, tick = FALSE, pos = (min(y) - (.1 / interval)), cex = cex.ax.lab)
+			axis(2, at = y, labels = y,tick = FALSE, pos = (min(x)), las = 2, cex = cex.ax.lab)
+			for (i in 1:(yn-1)){
+				text(genx, y[i] + 1, labels = (max(x) - y[i] - 1), srt = 45, xpd = TRUE, cex = cex.ax.lab)
 			}
-			text(genx+(.5/interval),mean(ages),"Birth Cohort",srt=270,cex=1.5,xpd=T)
+			text(genx + (.5 / interval), mean(ages), cohort.lab, srt = 270, cex = cex.lab, xpd = TRUE)
 		}
-	}
-	if (labs==F){
-		# again guessing which device to open
-		A <- try(windows(width=xrange+2,height=yrange+2),silent=T)
-		if (!is.null(A)) A <- try(Quartz(width=xrange+2,height=yrange+2),silent=T)
-		if (!is.null(A)) A <- try(x11(width=xrange+2,height=yrange+2),silent=T)
-		# start plotting
-		par(mai=c(0,0,0,0))
+	} else {
 		x <- years; y <- ages; xn <- length(x); yn <- length(y)
-		plot(NA,type="n",xlim=c(min(x),max(x)),ylim=c(min(y),max(y)),axes=F,xlab="",ylab="",cex.lab=1.5,asp=1,col=col)
-		segments(x,min(ages),x,max(ages),col=col);segments(min(years),y,max(years),y,col=col)
-		for (i in 1:(xn-1)) {
-    		segments(rep(x[i],(xn-1)),y[-yn],rep(x[i+1],(xn-1)),y[-1],col=col)
+		plot(NA,
+				type = "n",
+				xlim = range(x),
+				ylim = range(y),
+				axes = FALSE,
+				xlab = "",
+				ylab = "",
+				cex.lab = cex.lab,
+				asp = 1,
+				col = col)
+		segments(x, min(ages), x, max(ages), col = col)
+		segments(min(years), y, max(years), y, col = col)
+		for (i in 1:(xn - 1)) {
+    		segments(rep(x[i], (xn - 1)), y[-yn], rep(x[i + 1], (xn - 1)), y[-1], col = col)
     	}
 	}
 }
