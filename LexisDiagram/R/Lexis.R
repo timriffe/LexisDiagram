8e0e08fccb853f4c16db2b555514973fc72d0745
Tim Riffe <tim.riffe@gmail.com>
2016-08-22 10:44:23 -0700

    detects better, not consistent w past versions, oh well.

--- LexisDiagram/R/Lexis.R
+++ LexisDiagram/R/Lexis.R
@@ -1,15 +1,15 @@
 Lexis <-
-function(ages,
-		years,
-		labs = TRUE,
-		col = "black",
-		mar = c(1,1,1,1),
-		age.lab = "Age",
-		year.lab = "Year",
-		cohort.lab = "Birth Cohort",
-		cex.lab = 1,
-		cex.ax.lab = 1,
-		add = FALSE){
+		function(ages,
+				years,
+				labs = TRUE,
+				col = "black",
+				mar = c(1,1,1,1),
+				age.lab = "Age",
+				year.lab = "Year",
+				cohort.lab = "Birth Cohort",
+				cex.lab = 1,
+				cex.ax.lab = 1,
+				add = FALSE){
 	xrange <- diff(range(years))
 	yrange <- diff(range(ages))
 	maxrange <- max(c(xrange, yrange))
@@ -19,27 +19,26 @@ function(ages,
 	# open a device if ecessary, set margins
 	if (!add){
 		dev.new(width=xrange+2,height=yrange+2)
+		par(xaxs="i",yaxs="i",mar=mar)
 	}
-	# start plotting
-	par(xaxs="i",yaxs="i",mar=mar)
-	
 	if (labs){
 		# x coordinate for generations
 		interval <- 5 / max(c(length(ages), length(years)))
 		genx <- max(years) + .25 / interval
 		# year labels don't always fit:
 		x <- years; y <- ages; xn <- length(x); yn <- length(y)
-		
-		plot(NA, 
-				type = "n",
-				xlim = range(x),
-				ylim = range(y),
-				axes = FALSE, 
-				xlab = year.lab,
-				ylab = age.lab,
-				cex.lab = cex.lab,
-				asp = 1,
-				col = col)
+		if (!add){
+			plot(NA, 
+					type = "n",
+					xlim = range(x),
+					ylim = range(y),
+					axes = FALSE, 
+					xlab = year.lab,
+					ylab = age.lab,
+					cex.lab = cex.lab,
+					asp = 1,
+					col = col)
+		}
 		segments(x, min(ages), x,max(ages), col = col)
 		segments(min(years), y, max(years), y, col = col)
 		for (i in 1:(xn - 1)) {
@@ -53,21 +52,23 @@ function(ages,
 		}
 	} else {
 		x <- years; y <- ages; xn <- length(x); yn <- length(y)
-		plot(NA,
-				type = "n",
-				xlim = range(x),
-				ylim = range(y),
-				axes = FALSE,
-				xlab = "",
-				ylab = "",
-				cex.lab = cex.lab,
-				asp = 1,
-				col = col)
+		if (!add){
+			plot(NA,
+					type = "n",
+					xlim = range(x),
+					ylim = range(y),
+					axes = FALSE,
+					xlab = "",
+					ylab = "",
+					cex.lab = cex.lab,
+					asp = 1,
+					col = col)
+		}
 		segments(x, min(ages), x, max(ages), col = col)
 		segments(min(years), y, max(years), y, col = col)
 		for (i in 1:(xn - 1)) {
-    		segments(rep(x[i], (xn - 1)), y[-yn], rep(x[i + 1], (xn - 1)), y[-1], col = col)
-    	}
+			segments(rep(x[i], (xn - 1)), y[-yn], rep(x[i + 1], (xn - 1)), y[-1], col = col)
+		}
 	}
 }
 
